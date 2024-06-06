2d35d8a0b71f058ee215b339edbbe4e7512ed048
Delisma <delispence7@gmail.com>
2024-06-06 02:02:28 -0400

    added2

--- back-end/models.py
+++ back-end/models.py
@@ -121,3 +121,31 @@ class Porsche(db.Model, SerializerMixin):
         return image_url
     def __repr__(self):
         return f'Porsche {self.name}>'
+      
+class BMW(db.Model, SerializerMixin):
+      __tablename__ = 'BMWS'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(500))
+    description = db.Column(db.String(5000))
+    image_url = db.Column(db.String(500))
+    price = db.Column(db.Float)
+
+    @validates('name')
+    def validate_name(self, key, name):
+        if len(name) < 1:
+            raise ValueError('Name must be at least 1 character long')
+        return name
+
+    @validates('description')
+    def validate_description(self, key, description):
+        if len(description) < 1:
+            raise ValueError('Description must be at least 1 character long')
+        return description
+
+    @validates('image_url')
+    def validate_image_url(self, key, image_url):
+        if len(image_url) < 1:
+            raise ValueError('Image URL must be at least 1 character long')
+        return image_url
+    def __repr__(self):
+        return f'BMW {self.name}>'
